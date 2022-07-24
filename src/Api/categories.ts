92ad8003947879c78859b90bd6b59bb3199c6953
Grace Vargas <gracielavargasg@gmail.com>
2022-07-24 18:10:36 -0300

    add react-router-dom & Endpoints

---
+++ src/Api/categories.ts
@@ -0,0 +1,44 @@
+const addCategory = async (newCategory)=>{
+    const option = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newCategory)
+    }
+   await fetch('https://react-app-29176-default-rtdb.firebaseio.com/categories.json',option)
+}
+
+const deleteCategory = async (category) => {
+    const option = {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(category)
+    }
+   await fetch(`https://react-app-29176-default-rtdb.firebaseio.com/categories/${category}.json`,option)
+}
+
+
+
+const getCategories = async () => {
+    const response = await fetch('https://react-app-29176-default-rtdb.firebaseio.com/categories.json');
+    const data = await response.json();
+    return data;    
+  }
+
+
+const modifyCategory = async (category, modifiedCategory) => {
+  const option = {
+    method: 'PATCH',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(modifiedCategory)
+  }
+  await fetch(`https://react-app-29176-default-rtdb.firebaseio.com/categories/${category}.json`,option)
+}
+
+
+  export { addCategory, getCategories, modifyCategory, deleteCategory }
---
+++ src/Api/tasks.ts
@@ -0,0 +1,46 @@
+const addTask = async (newTask)=>{
+    const option = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newTask)
+    }
+   await fetch('https://react-app-29176-default-rtdb.firebaseio.com/tasks.json',option)
+}
+
+const deleteTask = async (task) => {
+    const option = {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(task)
+    }
+   await fetch(`https://react-app-29176-default-rtdb.firebaseio.com/tasks/${task}.json`,option)
+}
+
+
+
+const getTasks = async () => {
+    const response = await fetch('https://react-app-29176-default-rtdb.firebaseio.com/tasks.json');
+    const data = await response.json();
+    return data;    
+  }
+
+
+const modifyTask = async (task, modifiedTask) => {
+  const option = {
+    method: 'PATCH',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(modifiedTask)
+  }
+  await fetch(`https://react-app-29176-default-rtdb.firebaseio.com/tasks/${task}.json`,option)
+}
+
+
+
+
+  export { addTask, getTasks, modifyTask, deleteTask }
