8cdaf8fceea9c64b669c79285277494353ed6b5f
palenquiana <ludmilapalenque@hotmail.com>
2022-07-27 13:19:01 -0300

    feat: Card component

---
+++ src/Pages/Tasks/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { FC } from "react";
+import { Card } from "../../Components";
+// import { Task } from './type-task'
+import {AddTask} from './AddTask'
+
+
+const Tasks:FC = () => {
+    return (
+        <>
+         <Card className="card-task">
+
+
+         </Card>
+        </>
+
+
+    )
+
+}
+export { Tasks } 
