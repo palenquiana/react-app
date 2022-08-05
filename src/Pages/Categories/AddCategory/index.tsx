e484279d54b9a721a2e76cd231eef89f2c4c7a47
palenquiana <ludmilapalenque@hotmail.com>
2022-08-05 16:42:45 -0300

    Feat: form add/edit category and route

--- src/Pages/Categories/AddCategory/index.tsx
+++ src/Pages/Categories/AddCategory/index.tsx
@@ -1,31 +1,9 @@
-import React from 'react';
-import { Button } from '../../../Components';
+import { AddEditCategory } from '../Form';
 
 const AddCategory = () => {
 
     return (
-        <div className="container card card-form primary-form p-4">
-            <h3 className="text-center">Agregar categorías</h3>
-            <form>
-                <div className='d-block flex-column container align-items-center' >
-                    <div className='flex-column'>
-                        <label htmlFor="nameCat" className="fw-semibold">Nombre</label>
-                        <input type="text" className="form-control"/>
-
-                    </div>
-                    <div className='flex-column'>
-                        <label htmlFor="nameSlug" className="fw-semibold">Slug</label>
-                        <input type="text" className="form-control" />
-                    </div>
-
-                    {/* <Button className='btn' onClick={()=>} text="Agregar" type='submit'/> */}
-
-                </div>
-              
-
-            </form>
-            
-        </div>
+        <AddEditCategory title="Agregar categoría" />     
     )
 }
 
--- src/Pages/index.ts
+++ src/Pages/index.ts
@@ -2,4 +2,4 @@ export { ViewTasks, AddTask, OutletTasks,EditTask } from './Tasks'
 export { ViewUser,AddUser, OutletUser,EditUser } from "./Users"
 export { Register } from "./Register"
 export { Login } from "./Login"
-export { AddCategory,OutletCategories,ViewCategories } from "./Categories"
+export { AddCategory,OutletCategories,ViewCategories,EditCategory } from "./Categories"
